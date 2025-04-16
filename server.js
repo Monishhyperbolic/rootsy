eea74d8fb8e136c756a8547a040f8bcfd6a29b5b
Monish Patil <161637813+Monishhyperbolic@users.noreply.github.com>
2025-04-16 22:02:23 +0530

    f

---
+++ server.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const connection = mysql.createConnection({
+  host: 'your-cloud-sql-ip',
+  user: 'your-db-username',
+  password: 'your-db-password',
+  database: 'your-db-name',
+  port: 3306 // default for MySQL
+});
+
+app.get('/', (req, res) => {
+  connection.query('SELECT * FROM products', (err, results) => {
+    if (err) {
+      res.status(500).send('Error connecting to the database');
+    } else {
+      res.status(200).json(results); // Send the results as JSON
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
