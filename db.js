192aca1fb713c44fbc9f7b80ea475964725fb1b2
Monish Patil <161637813+Monishhyperbolic@users.noreply.github.com>
2025-04-17 22:03:34 +0530

    ef

--- db.js
+++ db.js
@@ -1,12 +1,8 @@
-const { Pool } = require('pg');
 require('dotenv').config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
